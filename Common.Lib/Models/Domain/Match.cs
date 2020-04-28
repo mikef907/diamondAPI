43a0cf2f8df52c04f921840e14469c94cef77086
Michael Fullom <mfullom@ahfc.us>
2020-04-28 15:43:22 -0800

    Mapping update

--- Common.Lib/Models/Domain/Match.cs
+++ Common.Lib/Models/Domain/Match.cs
@@ -9,7 +9,7 @@ namespace Common.Lib.Models.DM
     {
         public Guid Id { get; set; }
         public GameType Type { get; set; }
-        public GameState State { get; set; }
+        public GameState GameState { get; set; }
         public List<PlayerMatch> PlayerMatches { get; set; }
         public Guid? WinnerId { get; set; }
         public Player Winner { get; set; }
